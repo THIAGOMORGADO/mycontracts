5f9d5555b81f2ff6fa58725593d8609f50f179d2
thiago morgado <thiago_morgado@hotmail.com>
2023-11-07 03:09:03 -0300

    controller and models user

--- api/src/config/db.js
+++ api/src/config/db.js
@@ -1,14 +1,9 @@
 const { Sequelize } = require("sequelize")
 
-const sequelize = new Sequelize(
-  process.env.DB_NAME,
-  process.env.DB_USER,
-  process.env.DB_PASS,
-  {
-    host: process.env.DB_HOST,
-    dialect: "mysql",
-  }
-)
+const sequelize = new Sequelize("mycontracts", "root", "", {
+  host: process.env.DB_HOST,
+  dialect: "mysql",
+})
 
 sequelize
   .authenticate()
