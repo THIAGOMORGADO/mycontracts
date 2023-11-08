4822fd9ebcd23718b4ca6936650e9e647e60e0ed
thiago morgado <thiago_morgado@hotmail.com>
2023-11-08 07:16:18 -0300

    create models clients

--- mobile/App.js
+++ mobile/App.js
@@ -1,20 +1,28 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar"
+import { StyleSheet, Text, View } from "react-native"
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={styles.title}>
+        Open up App.js to start working on your app!
+      </Text>
       <StatusBar style="auto" />
     </View>
-  );
+  )
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#7149c1",
+    alignItems: "center",
+    justifyContent: "center",
   },
-});
+  title: {
+    fontWeight: "bold",
+    fontSize: 28,
+    color: "#fff",
+    textAlign: "center",
+  },
+})
